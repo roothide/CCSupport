c5e6dc27d4db240104230c43a6424457d81fad95
roothider <158083651+roothider@users.noreply.github.com>
2024-09-03 14:59:33 +0800

    add roothide support

--- Defines.h
+++ Defines.h
@@ -1,12 +1,12 @@
-#import <libroot.h>
+#import <roothide.h>
 
 #define DefaultModuleConfigurationPath @"/var/mobile/Library/ControlCenter/ModuleConfiguration.plist"
-#define CCSupportModuleConfigurationPath JBROOT_PATH_NSSTRING(@"/var/mobile/Library/ControlCenter/ModuleConfiguration_CCSupport.plist")
+#define CCSupportModuleConfigurationPath jbroot(@"/var/mobile/Library/ControlCenter/ModuleConfiguration_CCSupport.plist")
 #define DefaultModuleOrderPath @"/System/Library/PrivateFrameworks/ControlCenterServices.framework/DefaultModuleOrder~%@.plist"
 
-#define CCSupportBundlePath JBROOT_PATH_NSSTRING(@"/Library/Application Support/CCSupport")
-#define CCSupportModulesPath JBROOT_PATH_NSSTRING(@"/Library/ControlCenter/Bundles")
-#define CCSupportProvidersPath JBROOT_PATH_NSSTRING(@"/Library/ControlCenter/CCSupport_Providers")
+#define CCSupportBundlePath jbroot(@"/Library/Application Support/CCSupport")
+#define CCSupportModulesPath jbroot(@"/Library/ControlCenter/Bundles")
+#define CCSupportProvidersPath jbroot(@"/Library/ControlCenter/CCSupport_Providers")
 
 #define iOS15_WhitelistedFixedModuleIdentifiers @[@"com.apple.replaykit.AudioConferenceControlCenterModule", @"com.apple.replaykit.VideoConferenceControlCenterModule"]
 
